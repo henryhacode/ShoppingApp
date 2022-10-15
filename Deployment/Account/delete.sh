fd9daf622d6703729066a3c60d041c948089458b
Thiep Ha - Henry <thiep.ha@miu.edu>
2022-10-14 20:02:57 -0500

    add account deployment

---
+++ Deployment/Account/delete.sh
@@ -0,0 +1,3 @@
+kubectl delete -f AccountService.yml
+kubectl delete -f AccountDeployment.yml
+kubectl delete -f AccountConfig.yml
---
+++ Deployment/Account/deploy.sh
@@ -0,0 +1,3 @@
+kubectl apply -f AccountService.yml
+kubectl apply -f AccountDeployment.yml
+kubectl apply -f AccountConfig.yml
