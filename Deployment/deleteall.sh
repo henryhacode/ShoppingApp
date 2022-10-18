87031b7dbe98d69d42c1771e69d153559859dd4f
hong.thai <hong.thai@miu.edu>
2022-10-18 10:24:12 -0500

    fix deployment

---
+++ Deployment/deleteall.sh
@@ -0,0 +1,21 @@
+kubectl delete -f CommonConfig.yml
+
+kubectl delete -f Keycloak/KeycloakService.yml
+kubectl delete -f Keycloak/KeycloakDeployment.yml
+
+kubectl delete -f Account/AccountService.yml
+kubectl delete -f Account/AccountDeployment.yml
+
+kubectl delete -f Accountdb/AccountdbConfig.yml
+kubectl delete -f Accountdb/AccountdbService.yml
+kubectl delete -f Accountdb/AccountdbDeployment.yml
+
+kubectl delete -f Product/ProductService.yml
+kubectl delete -f Product/ProductDeployment.yml
+
+kubectl delete -f Productdb/ProductdbConfig.yml
+kubectl delete -f Productdb/ProductdbService.yml
+kubectl delete -f Productdb/ProductdbDeployment.yml
+
+kubectl delete -f Notification/NotificationService.yml
+kubectl delete -f Notification/NotificationDeployment.yml
