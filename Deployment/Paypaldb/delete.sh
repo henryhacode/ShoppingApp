d347df5d2646c6752ba7486c45d6e7bd04076189
Thiep Ha - Henry <thiep.ha@miu.edu>
2022-10-17 23:30:24 -0500

    add paypal db deployment

---
+++ Deployment/Paypaldb/delete.sh
@@ -0,0 +1,3 @@
+kubectl delete -f PaypaldbService.yml
+kubectl delete -f PaypaldbDeployment.yml
+kubectl delete -f PaypaldbConfig.yml
---
+++ Deployment/Paypaldb/deploy.sh
@@ -0,0 +1,3 @@
+kubectl apply -f PaypaldbConfig.yml
+kubectl apply -f PaypaldbDeployment.yml
+kubectl apply -f PaypaldbService.yml
