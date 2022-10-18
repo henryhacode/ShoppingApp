c621a9854ca2eb7d85df11fac3d05079af9325bc
hong.thai <hong.thai@miu.edu>
2022-10-18 07:56:17 -0500

    add deployment files for Product and Notification

--- Deployment/Keycloak/delete.sh
+++ Deployment/Keycloak/delete.sh
@@ -1,3 +1,2 @@
 kubectl delete -f keycloakService.yml
 kubectl delete -f keycloakDeployment.yml
-kubectl delete -f keycloakConfig.yml
--- Deployment/Keycloak/deploy.sh
+++ Deployment/Keycloak/deploy.sh
@@ -1,5 +1,4 @@
 kubectl apply -f keycloakService.yml
 kubectl apply -f keycloakDeployment.yml
-kubectl apply -f keycloakConfig.yml
 
 #minikube start --mount-string="kc_data:/opt/keycloak/data/import" --mount
