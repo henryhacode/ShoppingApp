3e94e2c1a43149c84004dff5f5fda1787d091b35
Thiep Ha - Henry <thiep.ha@miu.edu>
2022-10-18 22:36:06 -0500

    correct deployment scripts

--- Deployment/deployall.sh
+++ Deployment/deployall.sh
@@ -1,22 +1,8 @@
 kubectl apply -f CommonConfig.yml
 
-kubectl apply -f Keycloak/KeycloakConfig.yml
-kubectl apply -f Keycloak/KeycloakService.yml
-kubectl apply -f Keycloak/KeycloakDeployment.yml
-
-kubectl apply -f Accountdb/AccountdbConfig.yml
-kubectl apply -f Accountdb/AccountdbService.yml
-kubectl apply -f Accountdb/AccountdbDeployment.yml
-
-kubectl apply -f Account/AccountService.yml
-kubectl apply -f Account/AccountDeployment.yml
-
-kubectl apply -f Productdb/ProductdbConfig.yml
-kubectl apply -f Productdb/ProductdbService.yml
-kubectl apply -f Productdb/ProductdbDeployment.yml
-
-kubectl apply -f Product/ProductService.yml
-kubectl apply -f Product/ProductDeployment.yml
-
-kubectl apply -f Notification/NotificationService.yml
-kubectl apply -f Notification/NotificationDeployment.yml
+for dir in */ ; do
+    echo "$dir"
+    cd $dir
+    bash deploy.sh
+    cd ..
+done
