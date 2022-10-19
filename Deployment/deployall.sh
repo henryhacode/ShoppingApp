6add08fc091e85182e90998b8a9766f090a30111
Thiep Ha - Henry <thiep.ha@miu.edu>
2022-10-18 23:05:23 -0500

    better way to deploy all

--- Deployment/deployall.sh
+++ Deployment/deployall.sh
@@ -1,8 +1,30 @@
 kubectl apply -f CommonConfig.yml
 
-for dir in */ ; do
-    echo "$dir"
-    cd $dir
+services=(
+"Accountdb"
+"Account"
+"Bankdb"
+"Bank"
+"Creditdb"
+"Credit"
+"Keycloak"
+"Notification"
+"Orderdb"
+"Order"
+"Paymentdb"
+"Payment"
+"Paypaldb"
+"Paypal"
+"Productdb"
+"Product"
+"Shippingdb"
+"Shipping"
+)
+
+for svc in "${services[@]}"; do
+    echo "$svc"
+    cd $svc
     bash deploy.sh
     cd ..
 done
+
